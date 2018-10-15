7d8e89b11a1a5077feefb706481f1521dbb703fd
Zer00n <41852518@qq.com>
2018-10-15 08:13:28 +0800

    Update and rename pachong.py to pachongv2.py
    加注释，并解决big5编码里不包含的字符导致的错误。

--- pachongv2.py
+++ pachongv2.py
@@ -15,57 +15,56 @@ global titles
  
 #加载页面内容
 def load_page(url):
-
     try:
-        httpproxy_handler = urllib2.ProxyHandler({"http" : "127.0.0.1:1087"})
-        cj = cookielib.LWPCookieJar()
+        httpproxy_handler = urllib2.ProxyHandler({"http" : "127.0.0.1:1087"})    #本机开机的SS代理
+        cj = cookielib.LWPCookieJar()                                            #创建保存cookie的对象
         cookie_support = urllib2.HTTPCookieProcessor(cj)
-        opener = urllib2.build_opener(cookie_support, httpproxy_handler)
+        opener = urllib2.build_opener(cookie_support, httpproxy_handler)         #在使用urllib2访问url时，加入SS代理
         urllib2.install_opener(opener)
         user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0"
         headers = {"User-Agent" : user_agent}
         postData = {
-            ####抓包post到from的信息，账户密码等
+                #post的数据，通过burp抓包得知
             }
         postData = urllib.urlencode(postData)
         request = urllib2.Request(posturl, postData, headers)
         response = urllib2.urlopen(request)
         html = response.read() 
-        htmlc = html.decode("big5").encode("utf-8")
+        htmlc = html.decode("big5", 'ignore').encode("utf-8")   #出现big5转码错误，使用ignore忽略爆出的错误编码，直接以ASCII输出即可。
         h = urllib2.urlopen(url)
         htmlurl = h.read()
-        htmlurl2 = htmlurl.decode("big5").encode("utf-8")    ##转码
+        htmlurl2 = htmlurl.decode("big5", 'ignore').encode("utf-8")
 
         #print htmlurl2
-        #print h.getcode()   #状态码
+        print h.getcode()   #状态码
         print h.geturl()    #URL地址
         #print "--------------------------"
         getTitle(htmlurl2)
     except Exception,e:
         print str(e)
- 
+#循环
 def tieba_spider(url,startPage,endPage):
     for i in range(startPage,endPage + 1):
         page = (i - 1) * 1
         my_url = url + str(page)
         load_page(my_url)
-        print "--------第%d个----------" % i
+        print "%d--------------------" % i
  
 def getTitle(htmlurl2):
     global titles
-    info = re.findall(r'<td class="bold" width="65%">(.*?)</td>',htmlurl2,re.S)
+    info = re.findall(r'通过正则表达式，抓取页面中想抓取的信息',htmlurl2,re.S)
     for titleList in info:
         print titleList
-        print "---------------"
-    titles.write(" ".join(info))
+        #print "---------------"
+    titles.write(" ".join(info)+"\n")
  
 if __name__ == '__main__':
-    posturl = 'http://login'    ####登陆from的地址
-    url = "http://id"           ####需要爬取帖子的地址
-    startPage = 111111
-    endPage = 99999
+    posturl = 'post到服务器login的url'
+    url = "想爬取遍历的id="
+    startPage = 1111
+    endPage = 9999
     global titles 
-    titles = open('tt.txt','w')
+    titles = open('写入本地文件.txt','w')
     tieba_spider(url, startPage, endPage)
     titles.close()
     print "---------------------结束------------------"
